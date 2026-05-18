a5c87f40e003f803a014514ff5ffcbb559d6bcb9
Nereag443 <nerea12172005@gmail.com>
2026-05-18 13:58:43 +0200

    Reinstaladas dependencias

--- noteflow/metro.config.js
+++ noteflow/metro.config.js
@@ -1,3 +1,24 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
+config.resolver = {
+    ...config.resolver,
+    unstable_enablePackageExports: false,
+    resolveRequest: (context, moduleName, platform) => {
+        if (platform === 'web' && moduleName === 'zustand/middleware') {
+            return {
+                filePath: path.resolve(__dirname, 'node_modules/zustand/middleware.js'),
+                type: 'sourceFile',
+            };
+        }
+        return context.resolveRequest(context, moduleName, platform);
+    }
+}
+
+config.transformer = {
+    ...config.transformer,
+    unstable_transformProfile: 'default',
+};
+
+module.exports = config;
